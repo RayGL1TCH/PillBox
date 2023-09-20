9a36e7847eec3da2bd8e0000e1290a23e7ddbae9
thecoder-001 <ayush06feb@gmail.com>
2023-09-20 15:18:23 +0530

    More changes

--- project/models.py
+++ project/models.py
@@ -8,3 +8,15 @@ class User(UserMixin, db.Model):
     name = db.Column(db.String(1000))
     device_id = db.Column(db.String(100))
     device_data =db.Column(db.String(1000))
+    
+    medications = db.relationship('Medication', backref='user', lazy=True)
+
+
+class Medication(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    name = db.Column(db.String(100))
+    times = db.Column(db.JSON)  # Store medication times as JSON
+
+    def __repr__(self):
+        return f"Medication(name='{self.name}', times={self.times})"
