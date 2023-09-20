f8debc9fe1e1d5be8cf61a172249f2351b334ee2
thecoder-001 <ayush06feb@gmail.com>
2023-09-20 14:32:07 +0530

    More changes

---
+++ project/models.py
@@ -0,0 +1,10 @@
+from flask_login import UserMixin
+from . import db
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+    device_id = db.Column(db.String(100))
+    device_data =db.Column(db.String(1000))
