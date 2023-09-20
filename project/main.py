d9c3cd29f7731b3ca2ee07218f756331e8042017
thecoder-001 <ayush06feb@gmail.com>
2023-09-20 19:27:46 +0530

    More changes

--- project/main.py
+++ project/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Flask, Blueprint, render_template, send_file
 from flask_login import login_required, current_user
 from . import db
 
@@ -6,9 +6,15 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    return send_file('templates/index.html')
+    # return render_template('index.html')
 
 @main.route('/profile')
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.name)
+
+
+app = Flask(__name__)
+app.register_blueprint(main)
+app.static_folder = 'static'
--- project/models.py
+++ project/models.py
@@ -9,14 +9,14 @@ class User(UserMixin, db.Model):
     device_id = db.Column(db.String(100))
     device_data =db.Column(db.String(1000))
     
-    medications = db.relationship('Medication', backref='user', lazy=True)
+#     medications = db.relationship('Medication', backref='user', lazy=True)
 
 
-class Medication(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    name = db.Column(db.String(100))
-    times = db.Column(db.JSON)  # Store medication times as JSON
+# class Medication(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+#     name = db.Column(db.String(100))
+#     times = db.Column(db.JSON)  # Store medication times as JSON
 
-    def __repr__(self):
-        return f"Medication(name='{self.name}', times={self.times})"
+#     def __repr__(self):
+#         return f"Medication(name='{self.name}', times={self.times})"
